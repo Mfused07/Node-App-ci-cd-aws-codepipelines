df42663227928f6652f2a578fcbb110b459f8a6b
Mubashir Yasin <58633314+Mfused07@users.noreply.github.com>
2022-10-04 20:48:31 +0500

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,8 +3,6 @@ echo 'run after_install.sh: ' >> /home/ec2-user/test-app/deploy.log
 
 echo 'cd /home/ec2-user/test-app' >> /home/ec2-user/test-app/deploy.log
 cd /home/ec2-user/test-app >> /home/ec2-user/test-app/deploy.log
-pm2 start app.js
-pm2 save
 
 echo 'npm install' >> /home/ec2-user/test-app/deploy.log 
 npm install >> /home/ec2-user/test-app/deploy.log
